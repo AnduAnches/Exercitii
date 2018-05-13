91e5ece5aaa0b6e7e329e3caf049e21b81a86a7b
AnduAnches <anches.alexandru@gmail.com>
2018-05-13 13:40:50 +0300

    suma cifrelor dintr-un numar - metoda cu string

--- MoreExercises1/src/com/company/Exercise33.java
+++ MoreExercises1/src/com/company/Exercise33.java
@@ -1,23 +1,23 @@
-package com.company;
 
 import java.util.Scanner;
 
 public class Exercise33 {
 
-   public static void main(String[] args){
+    public static void main(String[] args) {
 
-            Scanner userInput = new Scanner(System.in);
-            System.out.println("Please insert integer number:");
-            String number = userInput.next();
-            int noOfDigits = number.length();
-            int[] currentElement = new int[noOfDigits];
-            int sum = 0;
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please insert integer number:");
+        String number = userInput.next();
+        int noOfDigits = number.length();
+        int [] currentElement = new int[noOfDigits];
+        int sum = 0;
 
-            for (int i=0; i < noOfDigits ; i++ ) {
-                currentElement[i] = Integer.parseInt(number[i]);
-                sum = sum + currentElement[i];
-            }
-            System.out.println("The sum of the number's digits is: " + sum);
+        for (int i = 0; i < noOfDigits; i++) {
+
+            currentElement[i] = Integer.parseInt(number.split("")[i]);
+            sum = sum + currentElement[i];
+        }
+        System.out.println("The sum of the number's digits is: " + sum);
     }
 
 }
